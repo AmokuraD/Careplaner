b23f5a1b63dc2ef392c491fd70be6cae41f3e4a4
AmokuraD <1575823782@qq.com>
2019-03-30 21:33:36 +0800

    careplaner

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+<el-container>
+    <el-heade>
+
+    </el-heade>
+    <el-main>
+
+    </el-main>
+</el-container>
+</template>
+<script>
+export default {
+    name:'Home'
+}
+</script>
+<style>
+
+</style>
---
+++ src/components/skill.vue
@@ -0,0 +1,25 @@
+<template>
+    <el-container>
+        <el-header></el-header>
+        <el-container>
+            <el-aside>
+                <el-menu @open="handleOpen" @close="handleClose">
+
+                </el-menu>
+            </el-aside>
+            <el-main></el-main>
+        </el-container>
+    </el-container>
+</template>
+<script>
+export default {
+    methods:{
+        handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+}
+</script>
