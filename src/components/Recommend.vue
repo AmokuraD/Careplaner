fa7090ee2988aea263293b3d1474a94009be9d87
AmokuraD <1575823782@qq.com>
2019-04-21 20:06:25 +0800

    axios

--- src/components/Recommend.vue
+++ src/components/Recommend.vue
@@ -4,15 +4,15 @@
       <el-card id="vedioR">
         <el-row>
           <el-col :span="6">
-            <!-- <img id="iJava"
+            <img id="iJava"
               :src="data.imgSrc"
-              alt=""> -->
+              alt="">
           </el-col>
           <el-col :span="18">
-            <!-- <p>{{data.title}}</p>
+            <p>{{data.title}}</p>
             <p>{{profession}}</p>
-            <p>{{name}}</p> -->
-            <Table style="margin-top:120px ;width:auto;align-self: center"></Table>
+            <p>{{name}}</p>
+            <!-- <Table style="margin-top:120px ;width:auto;align-self: center"></Table> -->
           </el-col>
         </el-row>
       </el-card>
@@ -38,29 +38,14 @@
   export default {
     components: {Table},
     data() {
-      // switch (this.$route.query.profession) {
-      //   case "Java":
-      //     return {
-      //       profession: this.$route.query.profession,
-      //       data: {
-      //         imgSrc: "https://edu-image.nosdn.127.net/B2B4D218926FB3D1D7A569B7D9EBADA4.png?imageView&thumbnail=426y240&quality=100",
-      //         title: "面向对象程序设计——Java语言"
-      //       }
-      //     }
-      //   default:
-      //     return {
-      //       name:this.$route.query.data,
-      //       profession: this.$route.query.profession,
-      //       data: {
-      //         imgSrc: '',
-      //         title: ''
-      //       }
-      //     }
-      // }
-      return{
-        info:null
-      }
-    },
+          return {
+            profession: this.$route.query.profession,
+            data: {
+              imgSrc: "https://edu-image.nosdn.127.net/B2B4D218926FB3D1D7A569B7D9EBADA4.png?imageView&thumbnail=426y240&quality=100",
+              title: "面向对象程序设计——Java语言"
+            }
+          }
+      },
     mounted(){
       this.axios.get("http://e74ez9.natappfree.cc/servlet/ProductServlet")
       .then(response=>(this.info=response))
